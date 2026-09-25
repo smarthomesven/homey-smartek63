bc9968915ae06ae669c351b3538a4dfedff474e1
smarthomesven <sven@dypodex.com>
2026-09-25 11:31:01 +0200

    Improved code

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const Homey = require('homey');
 
-module.exports = class MyApp extends Homey.App {
+module.exports = class SmartEK63App extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('SmartEK63 app has been initialized');
   }
 
 };
